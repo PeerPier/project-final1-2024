edaad71fad910a4c9a4be2899bc187871a5584bd
HelloPPiyaaa <piyarat.up@kkumail.com>
2024-10-08 15:23:47 +0700

    💕 trending post

--- client/src/Screens/home.page.tsx
+++ client/src/Screens/home.page.tsx
@@ -5,10 +5,13 @@ import { useEffect, useState } from "react";
 import Loader from "./loader.component";
 import { Post } from "../types/post";
 import BlogCard from "./blogpost.component";
+import MinimalBlogPost from "./nobanner-blog";
+import { MdAutoGraph } from "react-icons/md";
 
 const HomePage = () => {
   const API_URL = "http://localhost:3001";
   const [blogs, setBlogs] = useState<Post[] | null>(null);
+  const [trendingBlogs, setTrendingBlogs] = useState<Post[] | null>(null);
 
   const fetchLatestBlogs = () => {
     axios
@@ -21,8 +24,20 @@ const HomePage = () => {
       });
   };
 
+  const fetchTrendingBlogs = () => {
+    axios
+      .get(API_URL + "/posts/trending-blogs")
+      .then(({ data }) => {
+        setTrendingBlogs(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   useEffect(() => {
     fetchLatestBlogs();
+    fetchTrendingBlogs();
   });
 
   return (
@@ -52,10 +67,48 @@ const HomePage = () => {
                 })
               )}
             </>
-            <h1>ที่นี่บล็อกยอดนิยม</h1>
+            {trendingBlogs === null ? (
+              <Loader />
+            ) : (
+              trendingBlogs.map((blog, i) => {
+                return (
+                  <AnimationWrapper
+                    transition={{ duration: 1, delay: i * 0.1 }}
+                    key={i}
+                  >
+                    <MinimalBlogPost blog={blog} index={i} />
+                  </AnimationWrapper>
+                );
+              })
+            )}
           </InPageNavigation>
         </div>
-        <div className=""></div>
+        <div className="trending-blog">
+          <div className="d-flex flex-column gap-3 ">
+            <h1 className="fw-medium mb-2 fs-4">เรื่องราวที่อาจสนใจ</h1>
+          </div>
+
+          <div>
+            <h1 className="fw-medium mb-2 fs-5">
+              Trending <MdAutoGraph />
+            </h1>
+
+            {trendingBlogs === null ? (
+              <Loader />
+            ) : (
+              trendingBlogs.map((blog, i) => {
+                return (
+                  <AnimationWrapper
+                    transition={{ duration: 1, delay: i * 0.1 }}
+                    key={i}
+                  >
+                    <MinimalBlogPost blog={blog} index={i} />
+                  </AnimationWrapper>
+                );
+              })
+            )}
+          </div>
+        </div>
       </section>
     </AnimationWrapper>
   );
